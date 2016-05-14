2a7d588a0d2b8b8b8151eadd2eb6abd0ff4250ec
Vidovdan <filip.despotovic@gmail.com>
2016-05-14 13:00:58 +0200

    fixed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+
+	$("#navbarToogle").blur(function (event) {
+	var screenWidth = window.innerWidth;
+	if (screenWidth < 768){
+		$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
